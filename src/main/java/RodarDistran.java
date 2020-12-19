c2b6cebff0e00972150dc35c867f632d02beea95
charlesANC <charles.costa@gmail.com>
2020-12-19 18:11:09 -0300

    Colocando o código do Tiago Milo junto para poder compilar para SMA

---
+++ src/main/java/RodarDistran.java
@@ -0,0 +1,28 @@
+
+public class RodarDistran {
+
+	public static void main(String[] args) {
+		String[] rodarDistran = new String[] {
+				"-gui", 
+				" -agents " + 
+					"b1:br.unb.cic.comnet.distran.agents.broker.SequentialBroker(\"T\");" + 
+					"t1:br.unb.cic.comnet.distran.agents.transcoder.RandomTimeTranscoder(\"C\");" + 
+					"t2:br.unb.cic.comnet.distran.agents.transcoder.RandomTimeTranscoder(\"A\");" + 
+					"t3:br.unb.cic.comnet.distran.agents.transcoder.RandomTimeTranscoder(\"B\");" + 
+					"v1:br.unb.cic.comnet.distran.agents.viewer.SequentialPlayViewer;" + 
+					"v2:br.unb.cic.comnet.distran.agents.viewer.SequentialPlayViewer;" + 
+					"v3:br.unb.cic.comnet.distran.agents.viewer.SequentialPlayViewer;" + 
+					"v4:br.unb.cic.comnet.distran.agents.viewer.SequentialPlayViewer;" + 
+					"v5:br.unb.cic.comnet.distran.agents.viewer.SequentialPlayViewer;" + 
+					"v6:br.unb.cic.comnet.distran.agents.viewer.SequentialPlayViewer;" + 
+					"v7:br.unb.cic.comnet.distran.agents.viewer.SequentialPlayViewer;" + 
+					"v8:br.unb.cic.comnet.distran.agents.viewer.SequentialPlayViewer;" + 
+					"v9:br.unb.cic.comnet.distran.agents.viewer.SequentialPlayViewer;" + 
+					"v10:br.unb.cic.comnet.distran.agents.viewer.SequentialPlayViewer;" + 
+					"v11:br.unb.cic.comnet.distran.agents.viewer.SequentialPlayViewer;" + 
+					"v12:br.unb.cic.comnet.distran.agents.viewer.SequentialPlayViewer"};
+		
+		jade.Boot.main(rodarDistran);
+	}
+
+}
